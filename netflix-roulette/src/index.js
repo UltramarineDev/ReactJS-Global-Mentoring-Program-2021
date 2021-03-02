b3bd68e0f1bdfd0657f28fda3c4ecb2f9846419c
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-03-02 17:51:39 +0300

    cover with srtict mode.

--- netflix-roulette/src/index.js
+++ netflix-roulette/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import App from './components/App/App';
+import App from './components/App';
 import { render } from 'react-dom';
 
 render(
-  <App />,
+  <React.StrictMode> 
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
