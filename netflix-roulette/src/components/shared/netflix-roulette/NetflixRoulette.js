6ebdcf9ae08d3b14e29fa3d08df4943dfdd51057
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-03-04 23:48:37 +0300

    netflix roulette component.

---
+++ netflix-roulette/src/components/shared/netflix-roulette/NetflixRoulette.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './NetrflixRoulette.module.scss';
+
+const NetflixRoulette = () => (
+  <span className={styles.title}><strong>netflix</strong>roulette</span>
+);
+
+export default NetflixRoulette;
