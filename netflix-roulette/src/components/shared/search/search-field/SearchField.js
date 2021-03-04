6d1b5a7651675d10898a6756ce26a9798ab3a85f
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-03-04 23:49:00 +0300

    search component.

---
+++ netflix-roulette/src/components/shared/search/search-field/SearchField.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './SearchField.module.scss';
+
+const SearchFieldComponent = () => (
+  <input type="search" placeholder="What do you want to watch?"/>
+);
+
+export default SearchFieldComponent;
