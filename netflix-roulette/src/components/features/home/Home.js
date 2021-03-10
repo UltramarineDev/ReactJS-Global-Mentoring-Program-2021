24904f41d47300f5f883421536d6f58ca6fab6e4
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-03-10 08:47:50 +0300

    update Home component with acions and forms.

--- netflix-roulette/src/components/features/home/Home.js
+++ netflix-roulette/src/components/features/home/Home.js
@@ -4,6 +4,12 @@ import PropTypes from 'prop-types';
 import Header from '/src/components/shared/header';
 import NavBar from '/src/components/shared/navBar'
 import MovieList from '/src/components/shared/movies-list/MoviesList';
+import Modal from '/src/components/shared/modal';
+
+import AddMovieForm from '../movie-form';
+import EditMovieForm from '../movie-form';
+import DeleteMovieForm from '../delete-movie-form';
+
 import styles from './Home.module.scss';
 
 const HomeComponent = ({ 
@@ -15,9 +21,17 @@ const HomeComponent = ({
   sortOptions,
   onSortOptionChange,
   sortOptionId,
+  onAddMovie,
+  isAddMovieModalOpened,
+  onCloseAddMovie,
+  onActionClick,
+  actions,
+  onActionCancel,
+  isEditOpen,
+  isDeleteOpen,
   }) => (
   <>
-    <Header />
+    <Header onAddMovie={onAddMovie} />
     <div className={styles.movieListSection}>
       <NavBar
         tabs={tabs}
@@ -31,10 +45,20 @@ const HomeComponent = ({
       <div className={styles.moviesCount}><strong>{moviesCount}</strong> movies found</div>
       {filteredMovies &&  
         <div className={styles.movieList}>
-          <MovieList movies={filteredMovies}/>
+          <MovieList movies={filteredMovies} onActionClick={onActionClick} actions={actions}/>
         </div>
       }
     </div>
+
+    <Modal onClose={onCloseAddMovie} isOpen={isAddMovieModalOpened}>
+      <AddMovieForm />
+    </Modal>
+    <Modal onClose={onActionCancel} isOpen={isEditOpen}>
+      <EditMovieForm />
+    </Modal>
+    <Modal onClose={onActionCancel} isOpen={isDeleteOpen}>
+      <DeleteMovieForm />
+    </Modal>
   </>
 );
 
@@ -47,6 +71,19 @@ HomeComponent.propTypes = {
   sortOptions: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
   onSortOptionChange: PropTypes.func.isRequired,
   sortOptionId: PropTypes.string.isRequired,
+  onAddMovie: PropTypes.func.isRequired,
+  isAddMovieModalOpened: PropTypes.bool.isRequired,
+  onCloseAddMovie: PropTypes.func.isRequired,
+  onActionClick: PropTypes.func.isRequired,
+  action: PropTypes.string,
+  actions: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  onActionCancel: PropTypes.func.isRequired,
+  isEditOpen: PropTypes.bool.isRequired,
+  isDeleteOpen: PropTypes.bool.isRequired,
 };
+
+HomeComponent.defaultProps = {
+  action: '',
+}
 
 export default HomeComponent;
