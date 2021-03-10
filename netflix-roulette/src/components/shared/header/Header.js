9bd9a20c1c51deddb526637d948bc9f9a2b377d4
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-03-10 08:48:55 +0300

    onCLick to header.

--- netflix-roulette/src/components/shared/header/Header.js
+++ netflix-roulette/src/components/shared/header/Header.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { BUTTON_ICONS, BUTTON_TYPES } from '/src/components/shared/constants';
 import Search from '/src/components/shared/search';
@@ -7,17 +8,25 @@ import Button from '/src/components/shared/button'
 import NetflixRoulette from '/src/components/shared/netflix-roulette';
 import styles from './Header.module.scss';
 
-const HeaderComponent = () => (
+const HeaderComponent = ({ onAddMovie }) => (
   <div className={styles.searchSectionWrapper}>
    <div className={styles.searchSection}>
      <div className={styles.header}>
        <NetflixRoulette/>
-       <Button type={BUTTON_TYPES.ADD} iconType={BUTTON_ICONS.ADD} label='ADD MOVIE'/>
+       <Button 
+         type={BUTTON_TYPES.ADD} 
+         iconType={BUTTON_ICONS.ADD}
+         label='ADD MOVIE'
+         onClick={onAddMovie}/>
      </div>
      <div className={styles.searchDescription}>find your movie</div>
      <Search />
    </div>
   </div>
 );
+
+HeaderComponent.propTypes = {
+  onAddMovie: PropTypes.func.isRequired,
+};
 
 export default HeaderComponent;
