61b6820ccf5c0a05a12df0326488f7a3511716fc
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-03-10 09:05:04 +0300

    conficts.

--- netflix-roulette/src/components/layout/netflix-roulette/netflix-roulette.component.js
+++
@@ -1,7 +0,0 @@
-import styles from './component.scss';
-
-const NetflixRoulette = () => (
-  <span className={styles.title}><strong>netflix</strong>roulette</span>
-);
-
-export default NetflixRoulette;
